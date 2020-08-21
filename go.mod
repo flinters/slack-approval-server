085d7297220f6259d89cd0b752a5a0baf4b3aa78
Takashi Kawachi <t_kawachi@septeni-original.co.jp>
2020-08-21 18:04:00 +0900

    update

--- go.mod
+++ go.mod
@@ -3,12 +3,19 @@ module github.com/heroku/go-getting-started
 go 1.12
 
 require (
-	github.com/gin-gonic/gin v0.0.0-20150626140855-4cc2de6207f4
-	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
-	github.com/manucorporat/sse v0.0.0-20150604091100-c142f0f1baea // indirect
-	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
-	github.com/mattn/go-isatty v0.0.0-20150814002629-7fcbc72f853b // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20150629084131-d9558e5c97f8 // indirect
-	gopkg.in/bluesuncorp/validator.v5 v5.9.1 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gomodule/redigo v1.8.2
+	github.com/heroku/x v0.0.25
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
+	golang.org/x/sys v0.0.0-20200820212457-1fb795427249 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/bluesuncorp/validator.v5 v5.10.3 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,80 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gomodule/redigo/redis"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+func newPool(server string) *redis.Pool {
+
+	return &redis.Pool{
+
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.DialURL(server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+type EventCreateRequest struct {
+	TimeoutEpoch int `json:"timeout_epoch"`
+}
+
+type Event struct {
+	TimeoutEpoch int      `json:"timeout_epoch"`
+	Approvers    []string `json:"approvers"`
+}
+
+func MakeRandomStr(digit uint32) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// 乱数を生成
+	b := make([]byte, digit)
+	if _, err := rand.Read(b); err != nil {
+		return "", errors.New("unexpected error...")
+	}
+
+	// letters からランダムに取り出して文字列を生成
+	var result string
+	for _, v := range b {
+		// index が letters の長さに収まるように調整
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result, nil
+}
+
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
+	redisURL := os.Getenv("REDISTOGO_URL")
+	if redisURL == "" {
+		log.Fatal("REDISTOGO_URL must be set")
+	}
+
+	pool := newPool(redisURL)
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -24,6 +84,46 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	router.POST("/events", func(c *gin.Context) {
+		var req EventCreateRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		id, err := MakeRandomStr(16)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		conn := pool.Get()
+		defer conn.Close()
+
+		event := Event{
+			TimeoutEpoch: req.TimeoutEpoch,
+			Approvers:    []string{},
+		}
+
+		bytes, err := json.Marshal(&event)
+		eventString := string(bytes)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		_, err = conn.Do("SET", id, eventString)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, eventString)
+	})
+
+	// router.GET("/events/:id", func(c *gin.Context) {
+	// 	conn := pool.Get()
+	// 	defer conn.Close()
+	// 	id := c.Param("id")
+	// 	conn.
+	// })
 
 	router.Run(":" + port)
 }
