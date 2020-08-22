0c614cb49e987e5f0b68195ca7a5766ab57dc3d3
Takashi Kawachi <t_kawachi@septeni-original.co.jp>
2020-08-22 13:07:08 +0900

    update

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"time"
+
+	"bytes"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
@@ -49,13 +52,126 @@ func newPool(server string) *redis.Pool {
 }
 
 type EventCreateRequest struct {
-	TimeoutEpoch int `json:"timeout_epoch"`
+	TimeoutEpoch int64 `json:"timeout_epoch"`
 }
 
+type Status string
+
+const (
+	InProgress = Status("in-progress")
+	Approved   = Status("approved")
+	Rejected   = Status("rejected")
+	Timeout    = Status("timeout")
+)
+
 type Event struct {
-	Id           string   `json:"id"`
-	TimeoutEpoch int      `json:"timeout_epoch"`
+	ID           string   `json:"id"`
+	TimeoutEpoch int64    `json:"timeout_epoch"`
 	Approvers    []string `json:"approvers"`
+	Rejecters    []string `json:"rejecters"`
+	Status       Status   `json:"status"`
+}
+
+func NewEvent(timeoutEpoch int64) (*Event, error) {
+	id, err := MakeRandomStr(16)
+	if err != nil {
+		return nil, err
+	}
+
+	event := Event{
+		ID:           id,
+		TimeoutEpoch: timeoutEpoch,
+		Approvers:    []string{},
+		Status:       InProgress,
+	}
+
+	event.refreshStatus()
+
+	return &event, nil
+}
+
+func (event *Event) refreshStatus() {
+	if event.Status == "" {
+		event.Status = InProgress
+	}
+	if len(event.Rejecters) > 0 {
+		event.Status = Rejected
+	} else if len(event.Approvers) > 0 {
+		event.Status = Approved
+	} else if time.Now().After(time.Unix(event.TimeoutEpoch, 0)) {
+		event.Status = Timeout
+	}
+}
+
+func (event *Event) Approve(user string) {
+	event.Approvers = append(event.Approvers, user)
+	event.refreshStatus()
+}
+
+func (event *Event) Reject(user string) {
+	event.Rejecters = append(event.Rejecters, user)
+	event.refreshStatus()
+}
+
+// slack からくるメッセージのうち興味のある部分だけ
+type CallbackMessage struct {
+	User struct {
+		ID       string `json:"id"`
+		Username string `json:"username"`
+		Name     string `json:"name"`
+		TeamID   string `json:"team_id"`
+	} `json:"user"`
+	Message     map[string]interface{} `json:"message"`
+	ResponseURL string                 `json:"response_url"`
+	Actions     []struct {
+		BlockID string `json:"block_id"`
+		Value   string `json:"value"`
+	} `json:"actions"`
+}
+
+func (msg *CallbackMessage) IsApproved() bool {
+	return msg.Actions[0].Value == "1"
+}
+
+func (msg *CallbackMessage) EventID() string {
+	return msg.Actions[0].BlockID
+}
+
+func (msg *CallbackMessage) Validate() error {
+	if len(msg.Actions) == 0 {
+		return errors.New("actions is empty")
+	}
+	v := msg.Actions[0].Value
+	if v != "0" && v != "1" {
+		return fmt.Errorf("Unknown value: %s", v)
+	}
+	blocks, ok := msg.Message["blocks"]
+	if !ok {
+		return errors.New("message.blocks is not found")
+	}
+	switch blocks := blocks.(type) {
+	case []interface{}:
+		actionsCnt := 0
+		for _, elem := range blocks {
+			switch elem := elem.(type) {
+			case map[string]interface{}:
+				if elem["type"] == "actions" {
+					actionsCnt++
+				}
+			default:
+				return errors.New("A blocks element is not a map")
+			}
+		}
+		if actionsCnt == 0 {
+			return errors.New("No actions block found")
+		}
+		if actionsCnt > 1 {
+			return errors.New("2 or more actions blocks found")
+		}
+	default:
+		return errors.New("A blocks element is not an array")
+	}
+	return nil
 }
 
 func MakeRandomStr(digit uint32) (string, error) {
@@ -104,27 +220,11 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request to create an event"})
 			return
 		}
-		id, err := MakeRandomStr(16)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+		event, err := NewEvent(req.TimeoutEpoch)
+
 		conn := pool.Get()
 		defer conn.Close()
-
-		event := Event{
-			Id:           id,
-			TimeoutEpoch: req.TimeoutEpoch,
-			Approvers:    []string{},
-		}
-
-		bytes, err := json.Marshal(&event)
-		eventString := string(bytes)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		_, err = conn.Do("SET", id, eventString)
+		err = storeEvent(event, conn)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -135,15 +235,8 @@ func main() {
 	router.GET("/events/:id", func(c *gin.Context) {
 		conn := pool.Get()
 		defer conn.Close()
-
 		id := c.Param("id")
-		bytes, err := redis.Bytes(conn.Do("GET", id))
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		var event Event
-		err = json.Unmarshal(bytes, &event)
+		event, err := fetchEvent(id, conn)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -151,5 +244,89 @@ func main() {
 		c.JSON(http.StatusOK, event)
 	})
 
+	router.POST("/callback", func(c *gin.Context) {
+		payloadJson := c.PostForm("payload")
+		log.Println("payload", payloadJson)
+		var msg CallbackMessage
+		err := json.Unmarshal([]byte(payloadJson), &msg)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "payload is not JSON: " + payloadJson})
+			return
+		}
+		log.Println(msg)
+
+		if err = msg.Validate(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error() + ": " + payloadJson})
+			return
+		}
+		go processCallback(msg, pool)
+		c.String(http.StatusOK, "")
+	})
+
 	router.Run(":" + port)
+}
+
+func storeEvent(event *Event, conn redis.Conn) error {
+	bytes, err := json.Marshal(event)
+	eventString := string(bytes)
+	if err != nil {
+		return err
+	}
+	_, err = conn.Do("SET", event.ID, eventString)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fetchEvent(id string, conn redis.Conn) (*Event, error) {
+	bytes, err := redis.Bytes(conn.Do("GET", id))
+	if err != nil {
+		return nil, err
+	}
+	var event Event
+	err = json.Unmarshal(bytes, &event)
+	if err != nil {
+		return nil, err
+	}
+	event.refreshStatus()
+	return &event, nil
+}
+
+func formatMessage(msg CallbackMessage, event *Event) []byte {
+	return nil
+}
+
+func processCallback(msg CallbackMessage, pool *redis.Pool) {
+	conn := pool.Get()
+	defer conn.Close()
+	event, err := fetchEvent(msg.EventID(), conn)
+	if err != nil {
+		log.Println("Failed to get event:", msg)
+		return
+	}
+	userID := msg.User.ID
+	if msg.IsApproved() {
+		event.Approve(userID)
+	} else {
+		event.Reject(userID)
+	}
+	err = storeEvent(event, conn)
+	if err != nil {
+		log.Println("Failed to store event:", event)
+		return
+	}
+
+	json := formatMessage(msg, event)
+
+	res, err := http.Post(msg.ResponseURL, "application/json", bytes.NewBuffer(json))
+	if err != nil {
+		log.Println("Failed to post message:", err.Error())
+		return
+	}
+	if (res.StatusCode % 100) != 2 {
+		log.Println("Response status is not good:", res.Status)
+		return
+	}
+	log.Println("processCallback done")
 }
